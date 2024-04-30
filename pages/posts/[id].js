1f98cd17840d633524a24a554fe2f9a656c215d7
abspeight <abspeight@chapman.edu>
2024-04-29 19:36:17 -0700

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import Layout from '../../Components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../Components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+        <Date dateString={postData.date} />
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Layout>
+    );
+  }
+  
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
